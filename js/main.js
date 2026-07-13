4ffcb71685a0a10ad620bf0ce05513c82813f34c
NCreeper233 <nuclear.creeper_233@outlook.com>
2026-07-13 17:56:59 +0800

    commit

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const audioDataList = [
     { date: "2026年7月12日 星期日 23:27", src: "sounds/Desktop 2026.07.13 - 08.09.10.01.mp3" },
     { date: "2026年7月13日 星期一 14:55", src: "sounds/Desktop 2026.07.13 - 15.38.16.02.mp3" },
     { date: "2026年7月13日 星期一 14:55", src: "sounds/Desktop 2026.07.13 - 15.38.28.03.mp3" },
+    { date: "2026年7月13日 星期一 17:05", src: "sounds/Desktop 2026.07.13 - 17.49.55.04.mp3" },
+    { date: "2026年7月13日 星期一 17:17", src: "sounds/Desktop 2026.07.13 - 17.50.33.05.mp3" },
 ];
 
 const gridContainer = document.getElementById("audioGrid");
